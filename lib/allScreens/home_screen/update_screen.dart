82a7dd62e644a6b9261938a0b0e4e0bbbe19fa05
saki <faisalahmed423226@gmail.com>
2022-06-19 06:00:57 -0700

    new hello

---
+++ lib/allScreens/home_screen/update_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:untitled9/allScreens/home_screen/home_page.dart';
+class UpdateScreen extends StatelessWidget {
+  const UpdateScreen({Key? key,required UserList data}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
